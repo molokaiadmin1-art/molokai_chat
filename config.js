48a7328f4b9d1fb42930dc44da25de174122fa6c
molokaiadmin1-art <molokaiadmin1@gmail.com>
2026-04-19 14:51:13 -1000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+
+
+const CONFIG = {
+    // Paste your Web App URL between the quotes below
+    SHEET_URL: "https://script.google.com/macros/s/AKfycbyOu9mnYbFDwqnCIL1s_nZo3g9N7OGwlkANWktY1nP6hXScjmdKGVPRSFiVsSeYQTo/exec"
+};
+
+
+
+
+
+
+
+
+
+
