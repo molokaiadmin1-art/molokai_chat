f9a13dc88e3550a658978d9dbf174aea54804ff3
molokaiadmin1-art <molokaiadmin1@gmail.com>
2026-05-13 21:11:06 -1000

    Update SHEET_URL in config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 const CONFIG = {
     // Paste your Web App URL between the quotes below
-    SHEET_URL: "https://script.google.com/macros/s/AKfycbyOu9mnYbFDwqnCIL1s_nZo3g9N7OGwlkANWktY1nP6hXScjmdKGVPRSFiVsSeYQTo/exec"
+    SHEET_URL: "https://script.google.com/macros/s/AKfycby9jDpH7BwuYePfPG1lZIi233du_n69VgyE_nEAGo4Y0-f8Te51K3daVBHcaXfGqYmk/exec"
 };
 
 
